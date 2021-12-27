c43b6db8d96f0d5da6de86499b835e21419cbb99
Dusty Mumphrey <85353431+Dusttoo@users.noreply.github.com>
2021-12-27 17:04:55 -0600

    starting files

--- src/App.js
+++ src/App.js
@@ -1,23 +1,49 @@
 import logo from './logo.svg';
 import './App.css';
+import { useEffect, useState } from 'react';
 
 function App() {
+  const [loaded, setLoaded] = useState(false)
+  const [pokemon, setPokemon] = useState('')
+  const [species, setSpecies] = useState('')
+
+  useEffect(() => {
+    (async () => {
+    fetch("https://pokeapi.co/api/v2/pokemon/pikachu")
+      .then((res) => res.json())
+      .then((result) => {
+        setPokemon(result);
+      });
+
+    fetch(`https://pokeapi.co/api/v2/pokemon-species/${pokemon.id}/`)
+      .then((res) => res.json())
+      .then((result) => {
+        setSpecies(result)
+      })
+
+    })();
+    setLoaded(true)
+  }, [])
+
+  console.log(pokemon.sprites)
+  console.log(species)
+
+  
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {!loaded ? (
+        <h1>Where is Pikachu??</h1>
+      ) : (
+        <>
+          <h1>Here he is</h1>
+          <img
+            src={pokemon.sprites['front_default']}
+            alt={pokemon.name}
+          />
+        </>
+      )}
     </div>
   );
 }
