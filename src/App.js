40a22496e199fdad37c7651994c85609b0a2fd9d
Dusty Mumphrey <85353431+Dusttoo@users.noreply.github.com>
2021-12-28 11:32:40 -0600

    added interactive pokeball to main page

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import { useEffect, useState } from 'react';
 import Pokemon from './components/Pokemon';
+import './index.css'
 
 function App(pokedex) {
   const [loaded, setLoaded] = useState(false)
@@ -8,25 +9,33 @@ function App(pokedex) {
 
   return (
     <>
-      <h1>Who will you catch?!</h1>
+      <h1 className="main-header">Who will you catch?!</h1>
+      <p className="tagline">Click the pokeball to find out</p>
       {!pokeball ? (
-        <button
-          onClick={() => {
-            openPokeball(true);
-          }}
-        >
-          Throw Pokeball
-        </button>
+        <div className="pokeball-container">
+          <button
+            className="pokeball-button"
+            onClick={() => {
+              openPokeball(true);
+            }}
+          >
+            <img
+              className="pokeball-img"
+              src="http://www.pngmart.com/files/2/Pokeball-PNG-Photos.png"
+              alt="pokeball"
+            />
+          </button>
+        </div>
       ) : (
         <>
-        <Pokemon pokedex={pokedex}/>
-        <button
-          onClick={() => {
-            openPokeball(false);
-          }}
-        >
-          Throw Pokeball Again
-        </button>
+          <Pokemon pokedex={pokedex} />
+          <button
+            onClick={() => {
+              openPokeball(false);
+            }}
+          >
+            Throw Pokeball Again?
+          </button>
         </>
       )}
     </>
